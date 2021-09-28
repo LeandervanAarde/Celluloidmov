9332567646b44ce7cf5440694178eb1ced8a118e
Simon <170044@virtualwindow.co.za>
2021-09-29 00:38:35 +0200

    Sign up page for all devices
    Sign Up page for all devices, Have the sign up and log in for the large device. Still need to do the log in for the sm and md devices.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     console.log("JQuery is linked and ready");
 
+    $(".btn-click").on("click", function(){
+        $(".panel").toggleClass("panel-slider");
+        // $(".btn-click").html("Log In");
+        $(this).html(($(this).html() == "Log In") ? 'Sign Up' : 'Log In');
+    });
+
     
 });
