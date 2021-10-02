20c92ac285abddf312eb1803e367eaec4bf4e061
LeandervanAarde <200211@virtualwindow.co.za>
2021-10-02 16:29:33 +0200

    Individual movie progress
    Individual movie page general layout mostly done. Sections for more movies have been added and the footer is on track.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
     console.log("JQuery is linked and ready");
 
+    // Hover for Movie cards
     
+    $(".card").mouseenter(function(){
+        $(".card-body").removeClass("d-none");
+        $(".card-body").addClass("d-block");
+    });
+
+    $(".card").mouseleave(function(){
+        $(".card-body").removeClass("d-block");
+        $(".card-body").addClass("d-none");
+    });
 });
