0ba9c795f44d2212815ed35ed9993d559c9d3023
Simon <170044@virtualwindow.co.za>
2021-10-12 09:59:43 +0200

    Watchlist page done, just needs some styling.
    Watchlist page done, just needs some styling.

---
+++ js/watchlist.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    console.log("JQuery is linked and ready");
+
+       // Hover for Movie cards
+    
+       $(".card").mouseenter(function(){
+        $(".card-body").removeClass("d-lg-none");
+        $(".card-body").addClass("d-lg-block");
+    });
+
+    $(".card").mouseleave(function(){
+        $(".card-body").removeClass("d-lg-block");
+        $(".card-body").addClass("d-lg-none");
+    });
+
+    $(".dropdown").hide();
+
+
+
+    $(".burger").click(function(){
+        $(".dropdown").toggle(100);
+    });
+
+});
+    
