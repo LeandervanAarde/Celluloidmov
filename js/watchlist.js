57486026f5f9807eb0caf93b215cca448d84d2b3
LeandervanAarde <200211@virtualwindow.co.za>
2021-10-18 21:24:29 +0200

    changes
    more fine tuning on watchlist page

--- js/watchlist.js
+++ js/watchlist.js
@@ -1,25 +1,6 @@
 $(document).ready(function(){
     console.log("JQuery is linked and ready");
 
-       // Hover for Movie cards
-    
-       $(".card").mouseenter(function(){
-        $(".card-body").removeClass("d-lg-none");
-        $(".card-body").addClass("d-lg-block");
-    });
-
-    $(".card").mouseleave(function(){
-        $(".card-body").removeClass("d-lg-block");
-        $(".card-body").addClass("d-lg-none");
-    });
-
-    $(".dropdown").hide();
-
-
-
-    $(".burger").click(function(){
-        $(".dropdown").toggle(100);
-    });
 
 
     const queryString = window.location.search;
