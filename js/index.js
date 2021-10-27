67316d84920150db39c3380a23ba0bd672ad6f18
LeandervanAarde <200211@virtualwindow.co.za>
2021-10-27 08:21:46 +0200

    Commit
    Commit not made to changes

--- js/index.js
+++ js/index.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
     console.log("JQuery is linked and ready");
 
-    $(".dropdown").hide();
-
-    $(".burger").click(function(){
-        $(".dropdown").toggle(100);
     
-    });
-
 });
