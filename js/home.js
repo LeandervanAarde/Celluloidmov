5ba12331ecaf7d5c0da18d5e66b0a0b4658a0fcb
Simon <170044@virtualwindow.co.za>
2021-10-28 20:50:45 +0200

    Login and sign up password pattern added
    Login and sign up password pattern added

--- js/home.js
+++ js/home.js
@@ -6,21 +6,21 @@ var description = [];
 var backdrop = [];
 var movId = [];
 
-function sleep(time){
+function sleep(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
- async function changeImage(){
+async function changeImage() {
 
     $(".Movie-name").fadeOut(300);
     $(".mainM").fadeOut(300);
     $(".description").fadeOut(300);
-    $(".header-image").css("backgroundImage", "url("+backdrop[counter]+")");
+    $(".header-image").css("backgroundImage", "url(" + backdrop[counter] + ")");
     $(".header-image .btn").fadeOut(300);
-  
+
     await sleep(300);
 
-    if(localStorage.getItem('watchlater').includes(movId[counter])) {
+    if (localStorage.getItem('watchlater').includes(movId[counter])) {
         $(".header-image .watchlater-header").attr("disabled", "true");
         $(".header-image .watchlater-header").html("Added");
     } else {
@@ -30,7 +30,7 @@ function sleep(time){
 
     $(".Movie-name").html(names[counter]);
 
-    $(".mainM").html("<img id='mainCover' src='"+images[counter]+"' class='card-img-top img-fluid' alt=''>");
+    $(".mainM").html("<img id='mainCover' src='" + images[counter] + "' class='card-img-top img-fluid' alt=''>");
 
     $(".description").html(description[counter]);
     $(".header-image").attr("data-id", movId[counter]);
@@ -40,28 +40,28 @@ function sleep(time){
     $(".description").fadeIn(300);
     $(".header-image .btn").fadeIn(300);
 
-    counter ++;
- 
-   if (counter >= 3) {
-        counter = 0; 
+    counter++;
+
+    if (counter >= 3) {
+        counter = 0;
     }
 };
 
 // Check the localstorage onload.
-if(!localStorage.getItem("watchlater")) {
+if (!localStorage.getItem("watchlater")) {
     localStorage.setItem("watchlater", "");
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-// Welcome message if the user does exist in the session storage
-$('.welcome').html("Welcome, "+ sessionStorage.getItem("username"));
-// end of greeting
-const popularurl= "https://api.themoviedb.org/3/movie/popular?api_key=fbdaccb39dfca477ec685d5da0f0e705&language=en-US&page=1";
-    $.getJSON(popularurl, function(result) {
-        for(var i= 0; i < 8; i++){
-            var card = 
-                    "\
+    // Welcome message if the user does exist in the session storage
+    $('.welcome').html("Welcome, " + sessionStorage.getItem("username"));
+    // end of greeting
+    const popularurl = "https://api.themoviedb.org/3/movie/popular?api_key=fbdaccb39dfca477ec685d5da0f0e705&language=en-US&page=1";
+    $.getJSON(popularurl, function (result) {
+        for (var i = 0; i < 8; i++) {
+            var card =
+                "\
                         <div class='col-6 col-md-4 col-lg-3 card-container' style='border: none;'>\
                             <a href='I-movie.html?id=" + result.results[i].id + "'>\
                             <div class='card' style='border: none;' data-id='" + result.results[i].id + "'>\
@@ -78,12 +78,12 @@ const popularurl= "https://api.themoviedb.org/3/movie/popular?api_key=fbdaccb39d
                     ";
 
             // If already in localstorage append a card with disabled class that will state movie is already added
-            if(localStorage.getItem('watchlater').includes(result.results[i].id)) {
-                card = 
+            if (localStorage.getItem('watchlater').includes(result.results[i].id)) {
+                card =
                     "\
                         <div class='col-6 col-md-4 col-lg-3 card-container' style='border: none;'>\
                             <a href='I-movie.html?id=" + result.results[i].id + "'>\
-                            <div class='card' style='border: none;' data-id='" +result.results[i].id + "'>\
+                            <div class='card' style='border: none;' data-id='" + result.results[i].id + "'>\
                                 <img src='https://image.tmdb.org/t/p/original" + result.results[i].poster_path + "' class='card-img-top img-fluid' alt=''>\
                                 <div class='card-body d-block '>\
                                     <p class='cardName'><strong>" + result.results[i].original_title + "</strong> <br>Rating: " + result.results[i].vote_average + " <br> Release date: " + result.results[i].release_date + "</p> \
@@ -97,43 +97,43 @@ const popularurl= "https://api.themoviedb.org/3/movie/popular?api_key=fbdaccb39d
                     ";
             }
 
-        // for header
-        $(".body").append( card);
-            if(i < 3) {
+            // for header
+            $(".body").append(card);
+            if (i < 3) {
                 names.push(result.results[i].original_title);
                 images.push('https://image.tmdb.org/t/p/original' + result.results[i].poster_path);
-                description.push(result.results[i].overview);  
-                backdrop.push('https://image.tmdb.org/t/p/original' + result.results[i].backdrop_path)     
-                movId.push(result.results[i].id);      
+                description.push(result.results[i].overview);
+                backdrop.push('https://image.tmdb.org/t/p/original' + result.results[i].backdrop_path)
+                movId.push(result.results[i].id);
             }  //If statement ends
         }  //For loop ends 
 
         // Function to add to local storage.
-        $('.watchlater, .watchlater-header').click(function(event){
+        $('.watchlater, .watchlater-header').click(function (event) {
             let card = $(event.currentTarget).parent().parent()[0];
-            let id= $(card).data("id"); 
+            let id = $(card).data("id");
             var temp = localStorage.getItem('watchlater');
-            var watchlater ;
+            var watchlater;
 
-            if(temp == "") {
-                 watchlater = []; 
+            if (temp == "") {
+                watchlater = [];
             } else {
-                watchlater= temp.split(", ");
+                watchlater = temp.split(", ");
             }
 
-            var doesNotExist = watchlater.every(item =>{
+            var doesNotExist = watchlater.every(item => {
                 return item != id;
             });
 
-            if(doesNotExist ) {
-                watchlater.push(id + ""); 
+            if (doesNotExist) {
+                watchlater.push(id + "");
                 localStorage.setItem("watchlater", watchlater.join(", "));
             }
 
             $(event.currentTarget).attr("disabled", "true").html("Added");
-        }); 
+        });
 
-        $(".header-watch").on("click", () =>{
+        $(".header-watch").on("click", () => {
             var dataId = $(".header-image").data("id");
             window.location.href = "I-movie.html?id=" + dataId;
         });
@@ -147,21 +147,19 @@ const popularurl= "https://api.themoviedb.org/3/movie/popular?api_key=fbdaccb39d
 
 
 
-    usersUrl = "https://owmakerspace.co.za/users/data.json";
+    usersUrl = "https://owmakerspace.co.za/users/usersImages";
     var username = sessionStorage.getItem("username");
 
-    $.getJSON(usersUrl, function(result){
-        for(i = 0; i < result.users.length; i ++){
-            if(result.users[i].username === username) {
-                thePhoto = result.users[i].usersImages;
-                $(".photo").css("background-image", "url(" + thePhoto + ")");
+    $.getJSON(usersUrl, function (result) {
+        for (i = 0; i < result.users.length; i++) {
+            console.log(result.users[i].username);
+            if (result.users[i].username === username) {
+                // thePhoto = result.users[i].usersImages;
+                $(".photo").css("background-image", "url(" + result.users[i].usersImages + ")");
             }
         }; // for loops ends here 
 
     });
-
-
-
 
 
 
